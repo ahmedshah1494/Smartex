31cbc657ad1bb6ecc5d10090b13868db64bb4c15
Ahmed <shinigami1494@github.com>
2016-11-30 14:22:47 -0500

    fixed ui bugs

--- webapps/settings.py
+++ webapps/settings.py
@@ -25,7 +25,7 @@ PROJECT_ROOT = os.path.realpath(os.path.dirname(__file__)) + '/'
 SECRET_KEY = os.environ['SECRET_KEY']
 
 # SECURITY WARNING: don't run with debug turned on in production!
-# DEBUG = True
+DEBUG = True
 
 ALLOWED_HOSTS = []
 
